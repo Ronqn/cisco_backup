19460c1b85a0c41809c3d3c66881b1b4fafcb452
Ronan <ronandoudies@gmail.com>
2023-09-15 12:50:16 +0200

    Update cisco_backup.py
    Added ping function to check if the device is up.

--- cisco_backup.py
+++ cisco_backup.py
@@ -1,6 +1,10 @@
 from netmiko import ConnectHandler
 import getpass
 import csv
+import os
+
+def ping(address):
+    return os.system('ping %s -n 2' % (address))
 
 user = input("Enter the SSH username : ")
 passwd = getpass.getpass('Enter the password : ')
@@ -12,19 +16,24 @@ with open('switches.csv', 'r') as csvfile:
         csv_hostname = row[0]
         csv_device = row[1]
 
-        net_connect = ConnectHandler(
-            device_type="cisco_ios",
-            host=csv_device,
-            username=user,
-            password=passwd
-        )
+        alive = ping(csv_device)
 
-        hostname_output = net_connect.send_command("show conf | i hostname")
-        device_hostname = csv_hostname.replace('hostname ','')
+        if alive==0:
+            net_connect = ConnectHandler(
+                device_type="cisco_ios",
+                host=csv_device,
+                username=user,
+                password=passwd
+            )
 
-        running_config = net_connect.send_command("show run")
+            hostname_output = net_connect.send_command("show conf | i hostname")
+            device_hostname = csv_hostname.replace('hostname ','')
 
-        with open(f"{device_hostname}.txt", "w") as config_file:
-            config_file.write(running_config)
+            running_config = net_connect.send_command("show run")
 
-        net_connect.disconnect()
+            with open(f"{device_hostname}.txt", "w") as config_file:
+                config_file.write(running_config)
+
+            net_connect.disconnect()
+        else:
+            print("The host " + csv_hostname + " " + csv_device + " did not respond to the ping...")
