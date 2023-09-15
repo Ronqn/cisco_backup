4c81f4dd79e4461bed8016ee04221890419e3d17
Ronan <ronandoudies@gmail.com>
2023-09-15 13:54:50 +0200

    Update cisco_backup.py
    Added logging to an external file.

--- cisco_backup.py
+++ cisco_backup.py
@@ -2,9 +2,19 @@ from netmiko import ConnectHandler
 import getpass
 import csv
 import subprocess
+import logging
 
+#Ping function
 def ping(address):
     return subprocess.run(['ping',address,'-n','2'],stdout=subprocess.DEVNULL)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+loghandler = logging.FileHandler('cisco_backup.log')
+loghandler.setLevel(logging.INFO)
+logformat = logging.Formatter("%(asctime)s [%(levelname)s] - [%(filename)s] %(message)s",datefmt="%H:%M:%S")
+loghandler.setFormatter(logformat)
+logger.addHandler(loghandler)
 
 user = input("Enter the SSH username : ")
 passwd = getpass.getpass('Enter the password : ')
@@ -35,6 +45,8 @@ with open('switches.csv', 'r') as csvfile:
                 config_file.write(running_config)
 
             net_connect.disconnect()
+            logger.info("The running config of " + csv_hostname + " has been successfully saved.")
             print("The running config of " + csv_hostname + " has been successfully saved.")
         else:
+            logger.warning("The host " + csv_hostname + " " + csv_device + " did not respond to the ping... The config failed to save.")
             print("The host " + csv_hostname + " " + csv_device + " did not respond to the ping... The config failed to save.")
